783224416f175fdeb79a011f1934fced39c7cd29
JamesBond <introbond.dev@gmail.com>
2024-03-24 07:03:44 +0700

    Refactor Apollo Server imports and formatting

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,44 @@
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-import { gql } from 'apollo-server-express';
-import express from 'express';
-import http from 'http';
-import cors from 'cors';
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import { gql } from 'apollo-server-express'
+import express from 'express'
+import http from 'http'
+import cors from 'cors'
 
 const resolvers = {
     Query: {
         hello: (): string => 'Hello, world!',
     },
-};
+}
 
 const typeDefs = gql`
     type Query {
         hello: String
     }
-`;
+`
 
 interface MyContext {
-  token?: string;
+    token?: string
 }
 
-// Required logic for integrating with Express
-const app = express();
-// Our httpServer handles incoming requests to our Express app.
-// Below, we tell Apollo Server to "drain" this httpServer,
-// enabling our servers to shut down gracefully.
-const httpServer = http.createServer(app);
-
-// Same ApolloServer initialization as before, plus the drain plugin
-// for our httpServer.
+const app = express()
+const httpServer = http.createServer(app)
 const server = new ApolloServer<MyContext>({
-  typeDefs,
-  resolvers,
-  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-});
-// Ensure we wait for our server to start
-await server.start();
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+})
+await server.start()
 
-// Set up our Express middleware to handle CORS, body parsing,
-// and our expressMiddleware function.
 app.use(
-  '/',
-  cors<cors.CorsRequest>(),
-  express.json(),
-  // expressMiddleware accepts the same arguments:
-  // an Apollo Server instance and optional configuration options
-  expressMiddleware(server, {
-    context: async ({ req }) => ({ token: req.headers.token }),
-  }),
-);
+    '/',
+    cors<cors.CorsRequest>(),
+    express.json(),
+    expressMiddleware(server, {
+        context: async ({ req }) => ({ token: req.headers.token }),
+    }),
+)
 
-// Modified server startup
-await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
-console.log(`🚀 Server ready at http://localhost:4000/`);
+await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve))
+console.log(`🚀 Server ready at http://localhost:4000/`)
